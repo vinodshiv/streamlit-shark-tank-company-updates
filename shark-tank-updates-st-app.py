638118f38258b9be9a7027c3f56588affb10733c
Vinod Shivarudrappa <41847226+vinodshiv@users.noreply.github.com>
2024-12-07 21:18:36 -0800

    Update shark-tank-updates-st-app.py

--- shark-tank-updates-st-app.py
+++ shark-tank-updates-st-app.py
@@ -106,7 +106,7 @@ def extract_and_filter_paragraphs_html(content, include_keywords, exclude_keywor
 # Streamlit App
 st.set_page_config(page_title="🦈 Appeared on Shark Tank Updates", layout="centered")
 st.markdown("<h2 style='text-align: center; color: #315D94;'>🦈 \"Appeared on Shark Tank\" Updates</h2>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: #5B5D5F;'>Looks up abc.com for official companies on Shark Tank, gets current status update</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #5B5D5F;'>Find latest reported updates about companies that officially appeared on Shark Tank</p>", unsafe_allow_html=True)
 
 
 col1, col2, col3 = st.columns([0.1,3,0.1])
